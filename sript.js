edf1e83055b0cbfdd98d589c80b943d63d3846a0
mukalazimark <mukalazimarkeric24@gmail.com>
2022-12-30 08:31:42 -0800

    documenting

--- sript.js
+++ sript.js
@@ -7,27 +7,28 @@ Promise.all([
   faceapi.nets.faceExpressionNet.loadFromUri("./models"),
 ]).then(startVideo);
 
-function startVideo(){
-    navigator.mediaDevices.getUserMedia(
-        { video:{}},
-         stream => video.srcObject=stream
-        //  ,         err=console.log(err)
-    );
+function startVideo() {
+  navigator.mediaDevices.getUserMedia(
+    { video: {} },
+    (stream) => (video.srcObject = stream)
+    //  ,         err=console.log(err)
+  );
 }
 
-video.addEventListener('play',()=>{
-    const canvas = faceapi.createCanvasFromMedia(video);
-    document.body.append(canvas);
-    const displaySize = {width:video.width,height:video.height};
-    faceapi.matchDimensions(canvas,displaySize);
-    setInterval(async ()=>{
-        const detections=await faceapi.detectAllFaces(video,
-            new faceapi.TinyFaceDetectorOptions()).withFacelandmarks().withFaceExpressions()
-            const resizedDetections=faceapi.resizeResults(detections,displaySize);
-            canvas.getContext('2d').clearRect(0,0,canvas.width,canvas.height)
-            faceapi.draw.drawDetections(canvas,resizedDetections); 
-            faceapi.draw.drawFacelandmarks(canvas, resizedDetections);
-            faceapi.draw.drawFaceExpressions(canvas,resizedDetections);
-             
-    },100)
-})
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFacelandmarks()
+      .withFaceExpressions();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFacelandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+  }, 100);
+});
